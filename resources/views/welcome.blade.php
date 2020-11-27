f8b566039975d6f60ccfd4ae136fdf3f1db23b52
servidores-sistemas <servidores20@slp.tecnm.mx>
2020-11-26 19:33:12 -0600

    Mi nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,8 @@
         </style>
     </head>
     <body class="antialiased">
+        <h1 style="color:#FFF;">ISC. Stephanie Cordero Mart&iacute;nez</h1><br/>
+        
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
